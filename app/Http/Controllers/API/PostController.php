3c2cf3f8b1bd4ffa140909a29da9736bde3c0095
Satyam Shukla <shuklasatyam23056@gmail.com>
2024-06-03 19:06:27 +0530

    changes complete

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -40,33 +40,35 @@ class PostController extends Controller
     //         // return response()->json(['error' => $e->getMessage()], 500);
     //     }
     // }
-    
+
     public function store(Request $request)
     {
-        $login = Auth::user();
-        // dd($login->id);
-        $validator = Validator::make($request->all(), [
-            'content' => 'required|string',
-            'post_image' => 'nullable|image|mimes:jpeg,png,jpg,gif', // Adjust max file size as needed
-            // 'vendor_id' => 'required|exists:vendors,id',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400);
+        try {
+            $login = Auth::user();
+            $validator = Validator::make($request->all(), [
+                'content' => 'required|string',
+                'post_image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 400);
+            }
+            $post = new Post();
+            $post->content = $request->input('content');
+            $post->vendor_id = $login->id;
+            if ($request->hasFile('post_image')) {
+                $image = $request->file('post_image');
+                $imageName = time() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images/posts'), $imageName);
+                $post->post_image = $imageName;
+            }
+            $post->save();
+            return response()->json(['message' => 'Post created successfully', 'post' => $post], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Internal Server Error'], 500);
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-        $post = new Post();
-        $post->content = $request->input('content');
-        // $post->vendor_id = $request->input('vendor_id');
-        $post->vendor_id = $login->id;
-        if ($request->hasFile('post_image')) {
-            $image = $request->file('post_image');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('images/posts'), $imageName);
-            $post->post_image = $imageName;
-        }
-        $post->save();
-        return response()->json(['message' => 'Post created successfully', 'post' => $post], 201);
     }
-    
+
     public function update(Request $request)
     {
         try {
@@ -76,7 +78,7 @@ class PostController extends Controller
             }
             $validator = Validator::make($request->all(), [
                 'content' => 'required|string',
-                'post_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust max file size as needed
+                'post_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
                 'vendor_id' => 'required|exists:vendors,id',
             ]);
             if ($validator->fails()) {
@@ -111,11 +113,10 @@ class PostController extends Controller
                     'updated_at' => $post->updated_at,
                 ];
             })->toArray();
-            return response()->json(['message' => "All Posts",'posts' => $postinarray], 200);
+            return response()->json(['message' => "All Posts", 'posts' => $postinarray], 200);
         } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'Resource not found'], 404);
         } catch (\Exception $e) {
-            // return response()->json(['error' => $e->getMessage()], 500);
             return response()->json(['error' => 'Internal Server Error'], 500);
         }
     }
